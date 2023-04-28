77085fcd681c205adbf7bd8e6d497fad77039448
Giovanni Caiazzo <giovanni.caiazzo.work@gmail.com>
2023-04-28 12:08:00 +0200

    Forgot h-full

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -7,7 +7,7 @@ const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <CustomLayout invertDirection>
       <main className="h-screen w-screen flex flex-col min-h-0 bg-gray-200">
-        <div className="px-2 xs:px-6">{children}</div>
+        <div className="px-2 xs:px-6 h-full">{children}</div>
       </main>
     </CustomLayout>
   );
